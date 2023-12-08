90cc2b1f9c90f59797456c60675522491ea01f68
ruska- <ruska.beridze@gmail.com>
2023-12-08 18:29:33 +0400

    store page items

--- store.js
+++ store.js
@@ -1,5 +1,34 @@
 import items from "./items.json";
+import formatCurrency from "./util/formatCurrency";
+
+const IMAGE_URL = "https://dummyimage.com/600x360";
+
+//const container = document.querySelector("[data-store-container");
 
 console.log(items);
+items.forEach((item) => createItem(item));
+
+function createItem(item) {
+  const storeItemtemplate = document.querySelector("#store-item-template");
+
+  const storeItem = storeItemtemplate.content.cloneNode(true);
+
+  const container = document.querySelector("[data-store-container]");
+  container.dataset.id = item.id;
+
+  const name = storeItem.querySelector("[data-name]");
+  name.innerText = item.name;
+
+  const category = storeItem.querySelector("[data-category]");
+  category.innerText = item.category;
+
+  const image = storeItem.querySelector("[data-image]");
+  image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`;
+
+  const price = storeItem.querySelector("[data-price]");
+  price.innerText = formatCurrency(item.priceCents / 100);
+
+  container.appendChild(storeItem);
+}
 
 export function setupStore() {}
